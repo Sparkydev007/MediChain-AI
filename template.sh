36d895298641d8be46ab60d51a45ef91d17beeb9
Sparkydev007 <prathmeshbunde@gmail.com>
2026-03-18 23:52:39 +0530

    folder Structure Added

---
+++ template.sh
@@ -0,0 +1,16 @@
+mkdir -p src
+mkdir -p research
+
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch src .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+touch README.md
+
+
+echo "Directory structure created successfully!"
