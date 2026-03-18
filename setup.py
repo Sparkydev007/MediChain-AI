705ec6cbc5301392c6a8d35ef00b185fa593c940
Sparkydev007 <prathmeshbunde@gmail.com>
2026-03-19 00:18:40 +0530

    requirement added

---
+++ setup.py
@@ -0,0 +1,11 @@
+...
+from setuptools import find_packages, setup
+
+setup(
+    name="MediChain-AI",
+    version="0.1.0",
+    author="Prathmesh Bunde",
+    author_email="Prathmeshbunde@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
